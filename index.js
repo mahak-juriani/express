3129d0af4a2219880949ae76e3b14f3e3117512e
Mahak Juriani <juriani.mahak@gmail.com>
2024-07-03 11:22:55 +0530

    +middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(middleware);
+app.use(logger);
 
 let courses = [
     {id: 1, name: "java"},
@@ -23,6 +25,40 @@ app.post('/courses', (req, res)=>{
     courses.push(newCourse);
     res.send(courses);
 })
+
+app.put('./courses/:id', (req, res)=>{
+    try{
+        let newCourse = courses.find((course) => {
+            return course.id === +req.params.id
+        })
+
+        if(!newCourse){
+            res.status(404).sendStatus('course does not exist');
+        }
+
+        newCourse.name = req.body.name;
+        res.send(courses);
+    }
+
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+function middleware(req, res, next){
+    console.log("called");
+    next();
+}
+
+function logger(req, res, next){
+    const method = req.method;
+    const ip = req.ip;
+    const hostname = req.hostname;
+    const date = new Date().toISOString();
+
+    console.log(method, ip, hostname, date);
+    next();
+}
 
 app.listen(3001, ()=> {
     console.log("Server started.");
