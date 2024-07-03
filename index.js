c3b9fe701e91cefd70367bc2d4f6bf486480917c
Mahak Juriani <juriani.mahak@gmail.com>
2024-07-03 10:43:41 +0530

    post req

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [
     {id: 1, name: "java"},
-    {id:2, name: "javascript"},
-    {id:3, name: "python"}
+    {id: 2, name: "javascript"},
+    {id: 3, name: "python"},
 ];
 
 app.get('/courses', (req, res)=>{
     res.json(courses);
 })
 
-app.listen(3000, ()=> {
+app.post('/courses', (req, res)=>{
+    console.log(req.body);
+
+    let newCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(newCourse);
+    res.send(courses);
+})
+
+app.listen(3001, ()=> {
     console.log("Server started.");
 })
